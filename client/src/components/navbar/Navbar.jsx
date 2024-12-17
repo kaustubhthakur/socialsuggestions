dbd485736cc3dd2bb84777572827e3c19e190299
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-17 22:55:30 +0530

    second commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+// components/Navbar.js
+import React, { useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">MyLogo</div>
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
