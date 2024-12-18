c9907451db383e57f3255ef03131ee9820ac78d2
Kaustubh <kaustubhthakur76@gmail.com>
2024-12-18 20:52:00 +0530

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Navbar/>} />
+    </Routes>
   )
 }
 
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Logo</Link>
+      </div>
+      <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <Link to="/login" className="nav-link">Login</Link>
+        <Link to="/register" className="nav-link">Register</Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
